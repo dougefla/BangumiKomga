3e52272b776a7f151d0b1ed7413f221aa0c2bebd
初神 <45256288+chu-shen@users.noreply.github.com>
2025-05-05 18:55:24 +0800

    refactor: 修正数据源

--- updateReadProgress.py
+++ updateReadProgress.py
@@ -4,35 +4,57 @@ from tools.log import logger
 
 
 def update_read_progress():
-    '''
+    """
     更新阅读进度
-    '''
+    """
     env = InitEnv()
 
     bgm = env.bgm
-    all_series = env.all_series
+    komga = env.komga
+    series_list = []
 
-    for series in all_series:
-        series_name = series['name']
+    if KOMGA_LIBRARY_LIST and KOMGA_COLLECTION_LIST:
+        logger.error("Use only one of KOMGA_LIBRARY_LIST or KOMGA_COLLECTION_LIST")
+    elif KOMGA_LIBRARY_LIST:
+        series_list.extend(
+            komga.get_series_with_libraryid(KOMGA_LIBRARY_LIST)["content"]
+        )
+    elif KOMGA_COLLECTION_LIST:
+        series_list.extend(
+            komga.get_series_with_collection(KOMGA_COLLECTION_LIST)["content"]
+        )
+    else:
+        series_list = komga.get_all_series()["content"]
 
-        books_read_count = series['booksReadCount']
+    for series in series_list:
+        series_name = series["name"]
+
+        books_read_count = series["booksReadCount"]
         try:
-            for link in series['metadata']['links']:
-                if link['label'].lower() == "bangumi":
-                    subject_id = link['url'].split("/")[-1]
+            for link in series["metadata"]["links"]:
+                if link["label"].lower() == "bangumi":
+                    subject_id = link["url"].split("/")[-1]
                     break
         except ValueError as e:
             logger.exception(e)
-            logger.error("Update read progress for "+series_name+" failed:")
+            logger.error("Update read progress for " + series_name + " failed:")
             continue
 
         # TODO 添加是否同步判断逻辑，比如：是否`在读`
         if bgm.update_reading_progress(subject_id, books_read_count):
-            logger.info("Successfully update: "+series_name +
-                        " series read progress: "+str(books_read_count))
+            logger.info(
+                "Successfully update: "
+                + series_name
+                + " series read progress: "
+                + str(books_read_count)
+            )
         else:
-            logger.error("Failed to update: "+series_name +
-                         " series read progress: "+str(books_read_count))
+            logger.error(
+                "Failed to update: "
+                + series_name
+                + " series read progress: "
+                + str(books_read_count)
+            )
 
 
 update_read_progress()
