c4862894b2fe47b7a531dca2ec3ec6a271041f3d
WiiGe <WiiGe@users.noreply.github.com>
2025-06-05 13:36:28 +0800

    feat: 为logger添加调试器感知功能 (#93)
    * feat: 为logger添加调试器感知功能, 动态切换日志等级
    
    * chore: 调整日志输出
    
    ---------
    
    Co-authored-by: 初神 <45256288+chu-shen@users.noreply.github.com>

--- tools/log.py
+++ tools/log.py
@@ -1,6 +1,15 @@
 import sys
+import os
 import logging
 from logging.handlers import RotatingFileHandler
+
+
+def is_in_debug():
+    """检测是否在调试模式下运行"""
+    result = sys.gettrace()
+    logger.debug(f"调试器检测结果: {result}")
+    return bool(result)
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -15,8 +24,11 @@ fh = RotatingFileHandler(
     encoding="utf-8",
 )
 fh.setFormatter(formatter)
+fh.setLevel(logging.INFO)
 logger.addHandler(fh)
 
 sh = logging.StreamHandler(sys.stdout)
 sh.setFormatter(formatter)
+# 感知调试器, 切换日志等级
+sh.setLevel(logging.DEBUG if is_in_debug() else logging.INFO)
 logger.addHandler(sh)
